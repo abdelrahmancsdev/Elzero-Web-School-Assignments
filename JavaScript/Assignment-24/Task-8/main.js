edd7d23eeb685db52cfa88ed3ce399fb7b71523d
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2022-12-31 09:25:00 +0200

    Upload JavaScript Assignments

---
+++ JavaScript/Assignment-24/Task-8/main.js
@@ -0,0 +1,4 @@
+import calc from "./mod-one.js";
+import { modOne } from "./mod-two.js";
+
+console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
---
+++ JavaScript/Assignment-24/Task-8/mod-two.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 20;
+let c = 30;
+export let modOne = {
+  numOne: a,
+  numTwo: b,
+  numThree: c,
+};
